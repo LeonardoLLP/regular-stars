3b5fc6da09089ec6bc4a647b25d25bfbf841b86b
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2021-12-15 12:31:19 +0100

    Corrected small mistake X was giving me the interior angle, but I actually wanted the exterior angle, that is 180 - interior angle

--- stars.py
+++ stars.py
@@ -4,9 +4,9 @@ color = ('black', "white")
 # Radio
 r = 200
 # Número de lados
-n = 6
+n = 5
 # Número de giros
-full_rotations = 1
+full_rotations = 2
 
 """Proceso a seguir
 En teoría, podemos usar la fórmula de los polígonos regulares para calcular las rotaciones necesarias para realizar la estrella. La fórmula de los ángulos de un polígono regular es
@@ -16,10 +16,15 @@ Nosotros, al menos por ahora, usaremos:
 a = (180 * (n - 2) + 360 * (full_rotations - 1)) / n
 """
 
-a = (180 * (n - 2) + 360 * (full_rotations - 1)) / n
+x = (180 * (n - 2) + 360 * (full_rotations - 1)) / n
 
+a = 180 - x
+
+
+begin_fill()
 while True:
-    pass
+    forward(r)
+    left(a)
     if abs(pos()) < 1:
         break
 done()
