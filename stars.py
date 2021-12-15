a6a80145c16dac09c43a3fe1722ca82cdbf5815c
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2021-12-15 12:37:29 +0100

    Actualizado el docstring

--- stars.py
+++ stars.py
@@ -6,16 +6,21 @@ r = 200
 # Número de lados
 n = 5
 # Número de giros
-full_rotations = 2
+full_rotations = 1
 
 """Proceso a seguir
 En teoría, podemos usar la fórmula de los polígonos regulares para calcular las rotaciones necesarias para realizar la estrella. La fórmula de los ángulos de un polígono regular es
 a = (180 * (n - 2)) / n.
 
 Nosotros, al menos por ahora, usaremos:
-a = (180 * (n - 2) + 360 * (full_rotations - 1)) / n
+x = (180 * (n - 2) + 360 * (full_rotations - 1)) / n
+
+Esto nos dá el ángulo interior. Para pasarlo al ángulo exterior,
+
+a = 180 - x
 """
 
+# TODO: Full_rotations no funciona
 x = (180 * (n - 2) + 360 * (full_rotations - 1)) / n
 
 a = 180 - x
