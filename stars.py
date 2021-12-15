8108f08102dc812b436327d3d88613501722a5a3
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2021-12-15 12:47:37 +0100

    Hallada la fórmula Estaba pensando en dividir para cerrar el ángulo

--- stars.py
+++ stars.py
@@ -2,11 +2,11 @@ from turtle import *
 color = ('black', "white")
 
 # Radio
-r = 200
+r = 300
 # Número de lados
-n = 5
+n = 528
 # Número de giros
-full_rotations = 1
+full_rotations = 79
 
 """Proceso a seguir
 En teoría, podemos usar la fórmula de los polígonos regulares para calcular las rotaciones necesarias para realizar la estrella. La fórmula de los ángulos de un polígono regular es
@@ -18,18 +18,24 @@ x = (180 * (n - 2) + 360 * (full_rotations - 1)) / n
 Esto nos dá el ángulo interior. Para pasarlo al ángulo exterior,
 
 a = 180 - x
+
+Multiplicamos el ángulo interior por full_rotations. Esto hace que se cierre más cuantas más rotaciones hallamos especificado. Tiene que estar entre 1 y n
+
+Estaba pensando en dividir para cerrar en ángulo. Sin embargo, más grande a, más cerrado el ángulo interior (a determina rotación exterior)
 """
 
 # TODO: Full_rotations no funciona
-x = (180 * (n - 2) + 360 * (full_rotations - 1)) / n
+x = ((180 * (n - 2)) / n)
 
-a = 180 - x
+a = (180 - x) * full_rotations
+
 
 
 begin_fill()
+speed(0)
 while True:
     forward(r)
     left(a)
-    if abs(pos()) < 1:
+    if abs(pos()) < 0.1:
         break
 done()
